ba019b6f5f54b08d6e998d0e31bc7e03c89b8cf1
AlvinOt <thisisalvinhere@gmail.com>
2024-03-01 10:21:49 +0000

    registering customers orders

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # An online bakery shop
+
+Click [here](https://youtu.be/jZJ_SrdJjAY) to go the the Youtube tutorial on how to use this web app
--- myshopbakery/myshopbakery/settings.py
+++ myshopbakery/myshopbakery/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shopbakery.apps.ShopbakeryConfig',
     'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
